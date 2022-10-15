c5036fadd9a87390ff88f1ec92ad6ce16923d35b
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-10-15 12:47:10 +0200

    solving deploying issues
    ERROR: Could not find a version that satisfies the requirement pywin32==303 (from versions: none)

--- elawsuit/settings.py
+++ elawsuit/settings.py
@@ -52,6 +52,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
+
 ROOT_URLCONF = 'elawsuit.urls'
 
 TEMPLATES = [
